a5dd64fb0ed46289694bad53f851fda7916e45f5
Kasia <kasiaboekowska17@gmail.com>
2020-12-17 10:44:07 +0100

    Initial Commit

---
+++ lab4_zad10/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lab4_zad10
+//
+//  Created by Kasia on 17/12/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
